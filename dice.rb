f883f0ffd266984fe190dcbaf449b30f2cd9a916
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-15 21:56:37 +0000

    added erb files

--- dice.rb
+++ dice.rb
@@ -1,4 +1,5 @@
 # dice.rb
+# `bundle install`
 # run with `ruby dice.rb`
 
 require "sinatra"
@@ -14,14 +15,7 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 # ------------------------------------------------------------------ #
 
 get("/") do
-  "Hello World"
-
-  "<h1>Dice Roll</h1>
-
-  <li><a href='/dice/2/6'>Roll two 6-sided dice</a></li>
-  <li><a href='/dice/2/10'>Roll two 10-sided dice</a></li>
-  <li><a href='/dice/1/20'>Roll one 20-sided die</a></li>
-  <li><a href='/dice/5/4'>Roll five 4-sided dice</a></li>"
+  erb(:elephant)
 end
 
 # ------------------------------------------------------------------ #
@@ -43,10 +37,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 # ------------------------------------------------------------------ #

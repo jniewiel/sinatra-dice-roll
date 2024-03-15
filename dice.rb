aa4a6e0321a067744bf43c69fbfa369f06a9a6a4
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-15 21:14:51 +0000

    added error gem

--- dice.rb
+++ dice.rb
@@ -1,20 +1,39 @@
 # dice.rb
+# run with `ruby dice.rb`
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 # ------------------------------------------------------------------ #
 
 get("/") do
   "Hello World"
+
+  "<h1>Dice Roll</h1>
+
+  <li><a href='/dice/2/6'>Roll two 6-sided dice</a></li>
+  <li><a href='/dice/2/10'>Roll two 10-sided dice</a></li>
+  <li><a href='/dice/1/20'>Roll one 20-sided die</a></li>
+  <li><a href='/dice/5/4'>Roll five 4-sided dice</a></li>"
 end
 
-get("/zebra") do 
+# ------------------------------------------------------------------ #
+
+get("/zebra") do
   "We must add a route for each path we want to support"
 end
 
+# ------------------------------------------------------------------ #
+
 get("/giraffe") do
-"Hopefully this shows up without having to restart the server 🤞🏾"
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
 # ------------------------------------------------------------------ #
@@ -23,9 +42,9 @@ get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-	
+
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
@@ -36,9 +55,9 @@ get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-	
+
   outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
+
   "<h1>2d10</h1>
    <p>#{outcome}</p>"
 end
@@ -48,9 +67,9 @@ end
 get("/dice/1/20") do
   first_die = rand(1..20)
   sum = first_die
-	
+
   outcome = "You rolled a #{first_die} for a total of #{sum}."
-	
+
   "<h1>1d20</h1>
    <p>#{outcome}</p>"
 end
@@ -64,9 +83,9 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
+
   outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
-	
+
   "<h1>5d4</h1>
    <p>#{outcome}</p>"
 end
